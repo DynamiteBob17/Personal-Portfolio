6b7e7387ff4499a2dab19420f48e6ec20ed083a7
Jan Mlinarić <jan.mlinaric@gmail.com>
2022-11-27 23:12:20 +0100

    feat: app routes setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import WebProjects from "./pages/WebProjects";
+import JavaProjects from "./pages/JavaProjects";
+import CodingChallenges from "./pages/CodingChallenges";
+import NoPage from "./pages/NoPage";
 
 function App() {
-  return (
-    <div>
-      app
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="web_projects" element={<WebProjects />} />
+                    <Route path="java_projects" element={<JavaProjects />} />
+                    <Route path="coding_challenges" element={<CodingChallenges />} />
+                    <Route path="*" element={<NoPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
