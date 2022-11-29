1e7c5ed8c7350c464a589a77cf1d386776425cdd
Jan Mlinarić <jan.mlinaric@gmail.com>
2022-11-29 11:15:30 +0100

    feat: basic java_projects html done

--- src/pages/java_projects/JavaProjects.jsx
+++ src/pages/java_projects/JavaProjects.jsx
@@ -3,7 +3,64 @@ import './JavaProjects.css';
 
 function JavaProjects() {
     return (
-        <h1>Java Projects</h1>
+        <div id="java_projects_wrapper">
+            <div className={"java_project"}>
+                <a
+                    href="https://github.com/PickleEaterJim33/Sorting-Algorithms-Visualiser"
+                    target="_blank"
+                    rel="noreferrer"
+                >Sorting Algorithms Visualiser</a>
+                <iframe allowFullScreen src="https://drive.google.com/file/d/1OBKEh1TOtmTdD1VvAK5sXTFNmlONMX-H/preview" title="sav.mp4" />
+            </div>
+            <div className={"java_project"}>
+                <a
+                    href="https://github.com/PickleEaterJim33/Mandelbrot-Set-Viewer"
+                    target="_blank"
+                    rel="noreferrer"
+                >Mandelbrot Set Viewer</a>
+                <iframe allowFullScreen src="https://drive.google.com/file/d/11Mi2W3x3vyePwTL64MyzVsBZEsNGxaCp/preview" title="msv.mp4" />
+            </div>
+            <div className={"java_project"}>
+                <a
+                    href="https://github.com/PickleEaterJim33/CFR-Screensaver"
+                    target="_blank"
+                    rel="noreferrer"
+                >CFR Screensaver</a>
+                <iframe allowFullScreen src="https://drive.google.com/file/d/1uYW2UQEX0PoHeBSLv7oVA4yQz-T9b5E3/preview" title="cfr.mp4" />
+            </div>
+            <div className={"java_project"}>
+                <a
+                    href="https://github.com/PickleEaterJim33/Text-To-Speech"
+                    target="_blank"
+                    rel="noreferrer"
+                >Text-To-Speech</a>
+                <iframe allowFullScreen src="https://drive.google.com/file/d/194EXD6SjV-jnmp_D5llHuZjTQjCB5LF3/preview" title="tts.mp4" />
+            </div>
+            <div className={"java_project"}>
+                <a
+                    href="https://github.com/PickleEaterJim33/Calculator"
+                    target="_blank"
+                    rel="noreferrer"
+                >Calculator</a>
+                <iframe allowFullScreen src="https://drive.google.com/file/d/1lqzq6CyNdJlu45YbaQ2tcB3yGMiGcUqi/preview" title="calc.mp4" />
+            </div>
+            <div className={"java_project"}>
+                <a
+                    href="https://github.com/PickleEaterJim33/Snake-Game"
+                    target="_blank"
+                    rel="noreferrer"
+                >Snake Game</a>
+                <iframe allowFullScreen src="https://drive.google.com/file/d/1_zFf08T9Tt7NZCjts-2d6enNZWvgdRLn/preview" title="snake.mp4" />
+            </div>
+            <div className={"java_project"}>
+                <a
+                    href="https://github.com/PickleEaterJim33/Julia-Set-Viewer"
+                    target="_blank"
+                    rel="noreferrer"
+                >Julia Set Viewer</a>
+                <iframe allowFullScreen src="https://drive.google.com/file/d/1sisaNAs5P_Qg1J9SntBDiRlKrn8zfyUO/preview" title="jsv.mp4" />
+            </div>
+        </div>
     );
 }
 
