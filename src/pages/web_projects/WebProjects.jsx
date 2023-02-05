bc33a0e6ec98a9390bc695fb9bbb2917f5c64e47
PickleEaterJim33 <jan.mlinaric@gmail.com>
2023-02-05 17:02:14 +0100

    chore: add margin and move note to the left

--- src/pages/web_projects/WebProjects.jsx
+++ src/pages/web_projects/WebProjects.jsx
@@ -8,8 +8,8 @@ function WebProjects() {
             <p
                 style={{
                     position: 'absolute',
-                    right: '0',
-                    margin: '0',
+                    left: '0',
+                    margin: '7px',
                     fontSize: '13px',
                     fontStyle: 'italic',
                     color: 'grey',
