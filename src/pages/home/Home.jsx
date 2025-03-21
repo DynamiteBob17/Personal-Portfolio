79d04be6213f03b9b038a151bf879ae52850673a
Jan Mlinarić <jan.mlinaric@gmail.com>
2025-03-21 01:51:34 +0100

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -44,7 +44,7 @@ function Home() {
                         <i className={"fa-brands fa-github"} /> GitHub
                     </a>
                     <a
-                        href="https://europa.eu/europass/eportfolio/screen/share/f6ace12f-2e05-4a0c-bc38-22d43faedb4b?lang=en"
+                        href="https://europa.eu/europass/eportfolio/screen/share/3646c678-4dc3-45aa-8ad3-18d5e4e4a5df?lang=en"
                         target="_blank"
                         rel="noreferrer"
                     >
